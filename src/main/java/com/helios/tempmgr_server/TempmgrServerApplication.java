cf48ca9e77f1c046a628f90975911b34455fbd23
Alexandre Guillot <alexandre.guillot@posteo.net>
2024-07-01 21:19:05 +0200

    Initial commit

---
+++ src/main/java/com/helios/tempmgr_server/TempmgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.helios.tempmgr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempmgrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempmgrServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helios/tempmgr_server/TempmgrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helios.tempmgr_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempmgrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
